6cb5e97cfab196f05f6a6910e9b924632c1569c6
meatsby <meatsby@gmail.com>
2022-09-11 01:41:18 +0900

    feat: 1단계

--- thread/src/test/java/concurrency/stage1/UserServlet.java
+++ thread/src/test/java/concurrency/stage1/UserServlet.java
@@ -11,9 +11,16 @@ public class UserServlet {
         join(user);
     }
 
-    private void join(final User user) {
+    private synchronized void join(final User user) { // synchronized 를 통해 메서드에 접근할 수 있는 스레드를 제한
         if (!users.contains(user)) {
-            users.add(user);
+            // 스레드2 : !users.contains(user) = true 후 2초간 대기
+            // 스레드1 : !users.contains(user) = true 아직 users.add(user) 가 스레드2에 의해 호출되지 않음
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            users.add(user); // 두개의 스레드 모두 add 해버림
         }
     }
 

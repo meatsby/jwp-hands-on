fa9d8083109a51b7c5b2af8a7e714821604b9fc0
meatsby <meatsby@gmail.com>
2022-09-11 01:15:58 +0900

    feat: 0단계

--- thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
+++ thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
@@ -1,23 +1,19 @@
 package concurrency.stage0;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
- * 스레드 풀은 무엇이고 어떻게 동작할까?
- * 테스트를 통과시키고 왜 해당 결과가 나왔는지 생각해보자.
- *
- * Thread Pools
- * https://docs.oracle.com/javase/tutorial/essential/concurrency/pools.html
- *
- * Introduction to Thread Pools in Java
- * https://www.baeldung.com/thread-pool-java-and-guava
+ * 스레드 풀은 무엇이고 어떻게 동작할까? 테스트를 통과시키고 왜 해당 결과가 나왔는지 생각해보자.
+ * <p>
+ * Thread Pools https://docs.oracle.com/javase/tutorial/essential/concurrency/pools.html
+ * <p>
+ * Introduction to Thread Pools in Java https://www.baeldung.com/thread-pool-java-and-guava
  */
 class ThreadPoolsTest {
 
@@ -26,13 +22,14 @@ class ThreadPoolsTest {
     @Test
     void testNewFixedThreadPool() {
         final var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        // ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
         executor.submit(logWithSleep("hello fixed thread pools"));
         executor.submit(logWithSleep("hello fixed thread pools"));
         executor.submit(logWithSleep("hello fixed thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
-        final int expectedQueueSize = 0;
+        final int expectedPoolSize = 2; // 스레드풀을 2개로 제한함
+        final int expectedQueueSize = 1; // 3개의 명령을 submit 했으니 2개가 작업을 수행하고 하나는 큐에 쌓임
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
         assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
@@ -41,13 +38,14 @@ class ThreadPoolsTest {
     @Test
     void testNewCachedThreadPool() {
         final var executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        // ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
         executor.submit(logWithSleep("hello cached thread pools"));
         executor.submit(logWithSleep("hello cached thread pools"));
         executor.submit(logWithSleep("hello cached thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
-        final int expectedQueueSize = 0;
+        final int expectedPoolSize = 3; // 스레드풀이 Integer.MAX_VALUE 만큼 있음
+        final int expectedQueueSize = 0; // 스레드풀에서 필요한만큼 뽑아다 쓰기 때문에 큐에 쌓이지 않음
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
         assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
@@ -56,7 +54,9 @@ class ThreadPoolsTest {
     private Runnable logWithSleep(final String message) {
         return () -> {
             try {
-                Thread.sleep(1000);
+                log.info("start");
+                Thread.sleep(1000 * 10); // 10초 대기
+                log.info("end"); // 그 전에 메인 스레드가 종료
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }

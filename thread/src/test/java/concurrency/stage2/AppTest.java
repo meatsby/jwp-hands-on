0f48c27656dd28e08f66ee0cabfb1f5675f76411
meatsby <meatsby@gmail.com>
2022-09-11 02:01:54 +0900

    feat: 2단계

--- thread/src/test/java/concurrency/stage2/AppTest.java
+++ thread/src/test/java/concurrency/stage2/AppTest.java
@@ -13,12 +13,19 @@ class AppTest {
 
     /**
      * 1. App 클래스의 애플리케이션을 실행시켜 서버를 띄운다.
+     * <br>
      * 2. 아래 테스트를 실행시킨다.
+     * <br>
      * 3. AppTest가 아닌 App의 콘솔에서 SampleController가 생성한 http call count 로그를 확인한다.
+     * <br>
      * 4. application.yml에서 설정값을 변경해보면서 어떤 차이점이 있는지 분석해본다.
+     * <br>
      * - 로그가 찍힌 시간
+     * <br>
      * - 스레드명(nio-8080-exec-x)으로 생성된 스레드 갯수를 파악
+     * <br>
      * - http call count
+     * <br>
      * - 테스트 결과값
      */
     @Test
